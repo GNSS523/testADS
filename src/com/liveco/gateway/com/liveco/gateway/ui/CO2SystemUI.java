aaf7c80e09276d736bab526ec4c610ca79304c87
halsn <xuhalsn@gmail.com>
2017-04-01 11:26:51 +0800

    更新CO2SystemUI

--- src/com/liveco/gateway/com/liveco/gateway/ui/CO2SystemUI.java
+++ src/com/liveco/gateway/com/liveco/gateway/ui/CO2SystemUI.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.Objects;
 import java.util.Vector;
 
 /**
@@ -19,6 +20,17 @@ import java.util.Vector;
 public class CO2SystemUI extends JFrame{
     private CO2System s;
     private JPanel jp;
+    private JComboBox modeSelect;
+    private JLabel valveName;
+    private JLabel valvePLC;
+    private JButton valveOn;
+    private JButton valveOff;
+    private JLabel CO2SensorName;
+    private JLabel CO2SensorValue;
+    private JLabel minText;
+    private JLabel maxText;
+    private JTextField minValue;
+    private JTextField maxValue;
 
     public static void main(String[] args) {
         // write your code here
@@ -45,68 +57,116 @@ public class CO2SystemUI extends JFrame{
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setResizable(true);
         getContentPane().setLayout(null);
+        modeUI();
+        CO2ValveControlUI();
+        CO2SensorUI();
+        CO2ValveSettingUI();
+        setVisible(true);
+    }
+
+    private void modeUI() {
         Vector<String> vs = new Vector<String>();
         vs.add(String.valueOf(CO2SystemConstant.ModeCommand.AUTOMATIC));
         vs.add(String.valueOf(CO2SystemConstant.ModeCommand.MANUAL));
-        JComboBox modeSelect = new JComboBox(vs);
+        modeSelect = new JComboBox(vs);
         modeSelect.setBounds(100,100,100,50);
         // 设置模式
         modeSelect.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String mode = "" + modeSelect.getItemAt(modeSelect.getSelectedIndex());
-                //这里不知道用哪个函数
-                try {
-                    s.setMode(mode, CO2SystemConstant.ModeCommand.get(mode));
-                } catch (AdsException e1) {
-                    e1.printStackTrace();
-                }
-            }
-        });
-        JLabel valveName = new JLabel("Valve Name: " + CO2SystemConstant.Table.VALVE.getDescritpion());
-        JLabel valvePLC = new JLabel("Valve PLC: " + String.valueOf(CO2SystemConstant.Table.VALVE.getOffset()));
-        valveName.setBounds(100, 200, 300, 30);
-        valvePLC.setBounds(500, 200, 300, 30);
-        JButton valveOn = new JButton("Valve On");
-        JButton valveOff = new JButton("Vave Off");
-        valveOn.setBounds(100, 300, 100, 50);
-        valveOff.setBounds(300, 300, 100, 50);
-        JLabel CO2SensorName = new JLabel("Sensor Name: " + CO2SystemConstant.Table.CO2.getDescritpion());
-        JLabel CO2SensorValue = new JLabel("Sensor Value: " + String.valueOf(CO2SystemConstant.Table.CO2.getNumber()));
-        CO2SensorName.setBounds(100, 400, 300, 30);
-        CO2SensorValue.setBounds(500, 400, 300, 30);
-        byte minLimit = CO2SystemConstant.Table.CO2_LOWER_LIMIT.getNumber();
-        byte maxLimit = CO2SystemConstant.Table.CO2_HIGHER_LIMIT.getNumber();
-        // minLimt 和 maxLimit 的值一样
-        maxLimit += 10;
-        System.out.println(minLimit);
-        System.out.println(maxLimit);
-        JSlider LimitSlider = new JSlider(JSlider.HORIZONTAL,minLimit,maxLimit,minLimit);
-        JLabel minText = new JLabel("min: " + String.valueOf(minLimit));
-        JLabel maxText = new JLabel("max: " + String.valueOf(maxLimit));
-        JLabel sliderValue = new JLabel("value of slider: " + String.valueOf(minLimit));
-        LimitSlider.setBounds(100, 500, 300, 30);
-        minText.setBounds(50, 500, 50, 30);
-        maxText.setBounds(450, 500, 100, 30);
-        sliderValue.setBounds(100, 550, 300, 30);
-        LimitSlider.addChangeListener(new ChangeListener() {
-            @Override
-            public void stateChanged(ChangeEvent e) {
-                int foo = LimitSlider.getValue();
-                sliderValue.setText("value of slider: " + String.valueOf(foo));
+                //CO2System 这里不知道用哪个函数
+                //s.setMode(mode, CO2SystemConstant.ModeCommand.get(mode));
+                setMode(mode);
             }
         });
         getContentPane().add(modeSelect);
+    }
+
+    private void CO2ValveControlUI() {
+        valveName = new JLabel("Valve Name: " + CO2SystemConstant.Table.VALVE.getDescritpion());
+        valvePLC = new JLabel("Valve PLC: " + String.valueOf(CO2SystemConstant.Table.VALVE.getOffset()));
+        valveName.setBounds(100, 200, 300, 30);
+        valvePLC.setBounds(500, 200, 300, 30);
+        valveOn = new JButton("Valve On");
+        valveOff = new JButton("Vave Off");
+        valveOn.setBounds(100, 300, 100, 50);
+        valveOff.setBounds(300, 300, 100, 50);
+        valveOn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                valveOff.setEnabled(true);
+                valveOn.setEnabled(false);
+                // CO2System 打开valve
+                // 找不到那个函数
+                // s.setMode(CO2SystemConstant.Table.VALVE.);
+            }
+        });
+        valveOff.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                valveOn.setEnabled(true);
+                valveOff.setEnabled(false);
+                // CO2System 关闭valve
+            }
+        });
         getContentPane().add(valveName);
         getContentPane().add(valvePLC);
         getContentPane().add(valveOn);
         getContentPane().add(valveOff);
+    }
+
+    private void CO2SensorUI() {
+        CO2SensorName = new JLabel("Sensor Name: " + CO2SystemConstant.Table.CO2.getDescritpion());
+        CO2SensorValue = new JLabel("Sensor Value: " + String.valueOf(CO2SystemConstant.Table.CO2.getNumber()));
+        CO2SensorName.setBounds(100, 400, 300, 30);
+        CO2SensorValue.setBounds(500, 400, 300, 30);
         getContentPane().add(CO2SensorName);
         getContentPane().add(CO2SensorValue);
-        getContentPane().add(LimitSlider);
+    }
+
+    private void CO2ValveSettingUI() {
+        byte minLimit = CO2SystemConstant.Table.CO2_LOWER_LIMIT.getNumber();
+        byte maxLimit = CO2SystemConstant.Table.CO2_HIGHER_LIMIT.getNumber();
+        // minLimt 和 maxLimit 的值一样
+        maxLimit += 10;
+        minText = new JLabel("min: " + String.valueOf(minLimit));
+        maxText = new JLabel("max: " + String.valueOf(maxLimit));
+        minValue = new JTextField();
+        maxValue = new JTextField();
+        minText.setBounds(50, 500, 100, 30);
+        maxText.setBounds(450, 500, 100, 30);
+        minValue.setBounds(150, 500, 50, 30);
+        maxValue.setBounds(560, 500, 50, 30);
+        minValue.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                minText.setText("min: " + minValue.getText());
+            }
+        });
+        maxValue.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                maxText.setText("max: " + maxValue.getText());
+            }
+        });
         getContentPane().add(minText);
         getContentPane().add(maxText);
-        getContentPane().add(sliderValue);
-        setVisible(true);
+        getContentPane().add(minValue);
+        getContentPane().add(maxValue);
+    }
+
+    private void setMode(String m) {
+        if (Objects.equals(m, CO2SystemConstant.ModeCommand.AUTOMATIC.getDescritpion())) {
+            valveOn.setEnabled(false);
+            valveOff.setEnabled(false);
+            minValue.setEnabled(false);
+            maxValue.setEnabled(false);
+        } else if (Objects.equals(m, CO2SystemConstant.ModeCommand.MANUAL.getDescritpion())) {
+            valveOn.setEnabled(true);
+            valveOff.setEnabled(true);
+            minValue.setEnabled(true);
+            maxValue.setEnabled(true);
+        }
     }
 }
